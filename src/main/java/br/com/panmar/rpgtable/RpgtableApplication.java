4f36166b195cac97a58bb95cea8bcecfb8acc556
feassis <felipe.assis.2002@gmail.com>
2024-04-05 17:38:15 -0300

    sppring boot inital commit

---
+++ src/main/java/br/com/panmar/rpgtable/RpgtableApplication.java
@@ -0,0 +1,13 @@
+package br.com.panmar.rpgtable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgtableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgtableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/panmar/rpgtable/RpgtableApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.panmar.rpgtable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgtableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
